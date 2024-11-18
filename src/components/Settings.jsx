49fdfeb1ef4a3c6c2a0a3c719ab65ed859a228a6
Evelyn Bart <evelyn.m.bart@gmail.com>
2024-11-18 12:31:15 -0500

    install materialUI as react sliders weren't working

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -1,18 +1,9 @@
-import Slider from "rc-slider";
 import "../slider.css";
 
 export function Settings() {
   return (
     <div style={{ textAlign: "left" }}>
       <label>Work Minutes:</label>
-      <Slider
-        className="slider"
-        thumbClassName="thumb"
-        trackClassName="track"
-        value={45}
-        min={1}
-        max={120}
-      />
       <label>Break Minutes:</label>
     </div>
   );
