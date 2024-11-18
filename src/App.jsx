9d29c4b8876634b4f01e779655ff256625a5a501
Evelyn Bart <evelyn.m.bart@gmail.com>
2024-11-18 11:46:36 -0500

    create, flesh out and import different button components to Timer.jsx - finish Timer.jsx styles

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <main>
       <Timer />
-      <Settings />
+      {/* <Settings /> */}
     </main>
   );
 }
--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,7 +1,31 @@
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+import { PauseButton } from "./PauseButton";
+import { PlayButton } from "./PlayButton";
+import { SettingsButton } from "./SettingsButton";
+
+const red = "#f54e4e";
+const green = "#4aec8c";
+
 export function Timer() {
   return (
     <div>
-      <div>timer</div>
+      <CircularProgressbar
+        value={60}
+        text="60%"
+        styles={buildStyles({
+          textColor: "#fff",
+          pathColor: red,
+          trailColor: "rgba(255,255,255,.7)",
+        })}
+      />
+      <div style={{ marginTop: "20px" }}>
+        <PlayButton />
+        <PauseButton />
+      </div>
+      <div style={{ marginTop: "20px" }}>
+        <SettingsButton />
+      </div>
     </div>
   );
 }
