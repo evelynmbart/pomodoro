23bfc17263fc7b670e2ac2a4b5395adf84ebc7d3
Evelyn Bart <evelyn.m.bart@gmail.com>
2024-11-18 13:15:56 -0500

    fix setting sliders

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -6,18 +6,26 @@ export function Settings() {
     <div style={{ textAlign: "left" }}>
       <label>Work Minutes:</label>
       <Slider
-        min={45}
-        max={120}
-        className="slider"
-        aria-label="Working Minutes"
+        className="slider1"
+        defaultValue={30}
+        color="orange"
+        aria-label="Working Minutes Slider"
+        step={5}
+        marks
+        min={5}
+        max={60}
         valueLabelDisplay="auto"
       />
       <label>Break Minutes:</label>
       <Slider
-        min={45}
-        max={120}
         className="slider2"
-        aria-label="Working Minutes"
+        defaultValue={30}
+        color="teal"
+        aria-label="Working Minutes Slider"
+        step={5}
+        marks
+        min={5}
+        max={60}
         valueLabelDisplay="auto"
       />
     </div>
