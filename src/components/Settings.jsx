e2b0e55490173127a40b25a774bafe27a55416dc
Evelyn Bart <evelyn.m.bart@gmail.com>
2024-11-18 12:45:16 -0500

    using materialUI sliders, update styles

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -1,10 +1,25 @@
+import { Slider } from "@mui/material";
 import "../slider.css";
 
 export function Settings() {
   return (
     <div style={{ textAlign: "left" }}>
       <label>Work Minutes:</label>
+      <Slider
+        min={45}
+        max={120}
+        className="slider"
+        aria-label="Working Minutes"
+        valueLabelDisplay="auto"
+      />
       <label>Break Minutes:</label>
+      <Slider
+        min={45}
+        max={120}
+        className="slider2"
+        aria-label="Working Minutes"
+        valueLabelDisplay="auto"
+      />
     </div>
   );
 }
