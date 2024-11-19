fb807dc91ddc893601e733fae4891176036a6421
Evelyn Bart <evelyn.m.bart@gmail.com>
2024-11-19 12:13:58 -0500

    update task component; add animated cards

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,28 +1,15 @@
 import { Link } from "react-router-dom";
+import { Deck } from "./Deck";
 
 const tasks = ["clean room", "do laundry", "walk the dog", "call mom"];
 
-const to = (i: number) => ({
-  x: 0,
-  y: i * -4,
-  scale: 1,
-  rot: -10 + Math.random() * 20,
-  delay: i * 100,
-});
-const from = (_i: number) => ({ x: 0, rot: 0, scale: 1.5, y: -1000 });
-// This is being used down there in the view, it interpolates rotation and scale into a css transform
-const trans = (r: number, s: number) =>
-  `perspective(1500px) rotateX(30deg) rotateY(${
-    r / 10
-  }deg) rotateZ(${r}deg) scale(${s})`;
-
 export function Tasks() {
   return (
-    <div>
-      <div>tasks</div>
+    <div className="container">
       <Link id="link" to="/">
         Pomodoro
       </Link>
+      <Deck tasks={tasks} />
     </div>
   );
 }
