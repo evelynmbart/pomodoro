30409fc79140f99bdad0f7374cec7ead105b8df2
Evelyn Bart <evelyn.m.bart@gmail.com>
2025-01-22 11:30:46 -0500

    add localstorage use to maintain task items

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,11 +1,18 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { AddTaskButton } from "./Buttons/AddTaskButton";
 import { Deck } from "./Deck";
 import { NewTask } from "./NewTask";
 
 export function Tasks() {
   const [isNewTaskOpen, setIsNewTaskOpen] = useState(false);
-  const [tasks, setTasks] = useState([]);
+  const [tasks, setTasks] = useState(() => {
+    const savedTasks = localStorage.getItem("tasks");
+    return savedTasks ? JSON.parse(savedTasks) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
 
   const handleClick = () => {
     setIsNewTaskOpen(!isNewTaskOpen);
